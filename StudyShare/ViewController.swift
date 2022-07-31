d9fa2cabade081de5c11fca947bf8689bda763a0
Matthew Jennings <jenma048@student.otago.ac.nz>
2022-07-31 14:45:43 +1200

    Initial Commit

---
+++ StudyShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyShare
+//
+//  Created by Matthew Jennings on 31/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StudyShareUITests/StudyShareUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StudyShareUITestsLaunchTests.swift
+//  StudyShareUITests
+//
+//  Created by Matthew Jennings on 31/07/22.
+//
+
+import XCTest
+
+class StudyShareUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
